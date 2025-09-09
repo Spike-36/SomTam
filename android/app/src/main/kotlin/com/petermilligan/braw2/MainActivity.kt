9b78a0904e309e02a0e6084f4ccaa378797283b4
Spike-36 <peter.milligan@mac.com>
2025-09-09 10:13:08 +0900

    Initial clone of Braw3 for WordKimchi

--- android/app/src/main/kotlin/com/petermilligan/braw2/MainActivity.kt
+++ android/app/src/main/kotlin/com/petermilligan/braw2/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.braw3
+package com.brawapp2
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ android_old/app/src/main/kotlin/com/example/braw3/MainActivity.kt
@@ -0,0 +1,3 @@
+package com.example.braw3
+import io.flutter.embedding.android.FlutterActivity
+class MainActivity : FlutterActivity()
